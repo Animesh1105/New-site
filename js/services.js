6ec98be90dc28cc18e573a064c1104fafc3ef444
Abrar Shaha <73398955+shahabrar7746@users.noreply.github.com>
2024-02-25 23:00:16 +0000

    booking and listing is services tested

--- js/services.js
+++ js/services.js
@@ -1,6 +1,6 @@
 
 //pre processing 
-
+function loadContent(){
 
 dataArray = localStorage.getItem("json");
 dataArray = JSON.parse(dataArray);
@@ -8,17 +8,21 @@ dataArray = JSON.parse(dataArray);
            // Assuming the API returns an array of JSON objects
            dataArray.forEach((data, index) => {
             // Iterate through each JSON object
-        
+        var prevTag = document.getElementById("list-container").innerHTML;
             console.log(`Object ${index + 1}:`, data);
           //  document.getElementsByClassName("list-container").innerHTML =  `<h1>  ${data.service_name} </h1>`;
-         
+           document.getElementById("list-container").innerHTML = prevTag +  "<div class='card dark' ><div class='card-body'><h2 class='card-title fw-bold'>" + data.service_name + "</h2><h5 class='card-text mt-2'>   " + data.area + " " + data.state + " " + data.pin_code + "</h5><div class='cta-section'><h5 class='round mt-3'>" + data.service_type + "</h5></ div>  <div class='Callbutton'><h7 class='Callbutton mt-4'>" + data.phone_number + "</h7><button class='Callbutton' id = " + data.seller_id + " onclick='book(this)'>Book Appointment</button></div></div></div>";
             console.log('---'); // Separator for better readability
           });
       
+        }
+        window.onload = loadContent;
 
 
-
-
+function book(event){
+  var clickedElementId = event.id;
+  console.log("Clicked element ID: " + event.id);
+}
 
 
 async function doSearch(){
